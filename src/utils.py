abf2dd173c2a0e8e243b633925ebfeb23dda8cf6
OmarHossam50 <omarhossam50@proton.me>
2026-01-16 21:47:10 +0200

    feat: implement new_note function & make temp folder

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 def greeting(hour, fname):
         if hour > 4 and hour < 12:
                 return f"Good Morning {fname} ☀️"
@@ -7,3 +10,24 @@ def greeting(hour, fname):
                 return f"Good Evening {fname} ☺️"
         else:
                 return f"Good Night {fname} 🥰"
+
+def new_note():
+        working_dir = subprocess.run("pwd", capture_output=True, text=True).stdout
+        temp_file = f"{working_dir.strip()}/temp/new_note.md"
+        
+        print("TEMP: ", temp_file)
+        
+        # edit new_note temp file
+        os.system(f"micro {temp_file}")
+        
+        # save it's content
+        note_content = ""
+        with open(temp_file, "rt") as f:
+                 note_content = f.read()
+        
+        # remove the temp file
+        os.system(f"rm {temp_file}")
+        
+        # return the content
+        return note_content.strip()
+        
